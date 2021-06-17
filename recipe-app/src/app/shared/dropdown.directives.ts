f49b5a5697ce4a85a07f1b90b9db753e1cbd5297
MichaelEgli <michaelegli1879@gmail.com>
2021-06-17 13:48:42 +0200

    ex103

--- recipe-app/src/app/shared/dropdown.directives.ts
+++ recipe-app/src/app/shared/dropdown.directives.ts
@@ -11,3 +11,19 @@ export class DropdownDirective {
     }
     
 }
+
+/**
+ Folgende Variante würde das DropDown schliessen egal wo Du hinklicks (im document)
+ import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+ 
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
+}
+ */
