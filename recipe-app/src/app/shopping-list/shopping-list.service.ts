09da964e9f2d7f57b33ab866316dcb755475f821
MichaelEgli <michaelegli1879@gmail.com>
2021-09-08 14:41:18 +0200

    ex119 - my version - get ingredients from service

--- recipe-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,13 @@
-export class ShoppingList {
-    
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+    ingredients: Ingredient [] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+
+      getIncredients() {
+          return this.ingredients.slice();
+      }
+   // addIngredient method from shopping-list component
 }
